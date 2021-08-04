ef69bae30ead20f22eb3f48fcba912328683fc67
MascarBone <simone.bonelli@gmail.com>
2021-08-04 14:23:48 +0200

    Inizialized

---
+++ js/myscript.js
@@ -0,0 +1,19 @@
+var nameOut = prompt("Inserisci il tuo nome");
+console.log("nome = " + nameOut);
+
+var surnameOut = prompt("Inserisci il tuo cognome");
+console.log("nome = " + surnameOut);
+
+var colorOut = prompt("Inserisci il tuo colore preferito");
+console.log("nome = " + colorOut);
+
+var rans = parseInt(100 * Math.random());
+
+document.getElementById("nameOut").innerHTML += nameOut;
+document.getElementById("surnameOut").innerHTML += surnameOut;
+document.getElementById("colorOut").innerHTML += colorOut;
+
+
+document.getElementById("toShow").innerHTML =
+    document.getElementById("toShow").innerHTML +
+    nameOut + surnameOut + colorOut + rans;
